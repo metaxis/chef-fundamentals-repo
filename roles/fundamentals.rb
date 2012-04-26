c2a23e85ea1826fc8deb7dcb1e56f8e627c2a157
mattray <matthewhray@gmail.com>
2012-04-26 10:45:05 -0500

    switch to ruby 1.9.1

--- roles/fundamentals.rb
+++ roles/fundamentals.rb
@@ -1,7 +1,8 @@
 name "fundamentals"
 description "Default run_list for Fundamentals Showoff machine"
 run_list(
-  "recipe[rubygems::install]",
+  "recipe[ruby::1.9.1]",
+  "recipe[build-essential]",
   "recipe[showoff]",
   "recipe[fundamentals]"
   )
